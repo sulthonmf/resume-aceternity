de145c840f708a84eeb8383335d0812ff2169e3e
sulthonmf <sulthonmf@gmail.com>
2025-02-03 05:35:43 +0700

    remove webpack

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    config.node = {
+      fs: "empty",
+      child_process: "empty",
+      net: "empty",
+      dns: "empty",
+      tls: "empty",
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
