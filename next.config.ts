624d3b497284128ea79dcca06f3772b30aa1efba
sulthonmf <sulthonmf@gmail.com>
2025-02-03 04:35:03 +0700

    Fix config

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  basePath: "/src",
   webpack: (
     config,
     { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
   ) => {
     // Important: return the modified config
+    config.mode = dev ? 'development' : 'production';
     return config;
   },
 };
