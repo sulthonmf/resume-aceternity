7720bb88f51cac5338c0da49bcba8cb199253c32
sulthonmf <sulthonmf@gmail.com>
2025-02-03 05:02:06 +0700

    revert changes

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: true,
-  basePath: "/src",
-  webpack: (
-    config,
-    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-  ) => {
-    // Important: return the modified config
-    config.mode = dev ? 'development' : 'production';
-    return config;
-  },
 };
 
 export default nextConfig;
