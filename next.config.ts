2aaf4d64f13854970326c8a4a6fb02ba789382a7
sulthonmf <sulthonmf@gmail.com>
2025-02-03 04:27:47 +0700

    Add config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+    // Important: return the modified config
+    return config;
+  },
 };
 
 export default nextConfig;
